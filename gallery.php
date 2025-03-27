ec0d7bfa8a1f1dc1394a145cf941fb520d5d6bd0
syarifulanam <em.syariful.anam@gmail.com>
2025-03-27 13:14:32 +0700

    revisi fitur gallery

--- gallery.php
+++ gallery.php
@@ -98,7 +98,7 @@ require 'function-gallery.php';
                         </div>
                       </div>
                       <div class="modal-footer">
-                        <button type="button" class="btn btn-primary" data-dismiss="modal">Cancel</button>
+                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Cancel</button>
                         <button type="submit" class="btn btn-success" name="addGallery">Submit</button>
                       </div>
                     </form>
@@ -132,7 +132,7 @@ require 'function-gallery.php';
                     $i = 1;
                     while ($data = mysqli_fetch_array($ambilsemuadatagallery)) {
                       $name = $data['name'];
-                      $iduser = $data['iduser'];
+                      $id = $data['id'];
                       $description = $data['description'];
                       $picture = $data['picture'];
 
@@ -150,13 +150,12 @@ require 'function-gallery.php';
                         <td><?= $description; ?></td>
                         <td><?= $picture; ?></td>
                         <td>
-                          <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#edit<?= $iduser; ?>">Edit</button>
+                          <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#edit<?= $id; ?>">Edit</button>
                           <!-- Edit -->
-                          <div class="modal fade" id="edit<?= $iduser; ?>">
+                          <div class="modal fade" id="edit<?= $id; ?>">
                             <div class="modal-dialog">
                               <div class="modal-content">
-                                <form method="post" action="gallery.php">
-                                  <!-- Modal Header -->
+                                <form method="post" action="gallery.php" enctype="multipart/form-data">
                                   <div class="modal-header">
                                     <h4 class="modal-title">Gallery</h4>
                                     <!-- <button type="button" class="btn-close" data-bs-dismiss="modal"></button> -->
@@ -171,7 +170,7 @@ require 'function-gallery.php';
                                       <label class="form-label">Description</label>
                                       <input type="text" name="description" value="<?= $description; ?>" class="form-control" placeholder="enter a description">
                                     </div>
-                                    <input type="hidden" name="id" value="<?= $iduser; ?>">
+                                    <input type="hidden" name="id" value="<?= $id; ?>">
                                     <div class="mb-3">
                                       <label class="form-label">Picture</label>
                                       <input type="file" name="file" class="form-control">
@@ -186,9 +185,9 @@ require 'function-gallery.php';
                             </div>
                           </div>
 
-                          <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#delete<?= $iduser; ?>">Delete</button>
+                          <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#delete<?= $id; ?>">Delete</button>
                           <!-- Delete -->
-                          <div class="modal fade" id="delete<?= $iduser; ?>">
+                          <div class="modal fade" id="delete<?= $id; ?>">
                             <div class="modal-dialog">
                               <div class="modal-content">
                                 <form method="post" action="gallery.php">
@@ -199,14 +198,14 @@ require 'function-gallery.php';
                                   </div>
                                   <!-- Modal Body -->
                                   <div class="modal-body">
-
-                                    Are you sure you want to delete <?= htmlspecialchars($name); ?>?
-                                    <input type="hidden" name="id" value="<?= $iduser; ?>">
+                                    Are you sure you want to delete <b><?= htmlspecialchars($name); ?></b>? <br>
+                                    <b>ID: <?= $id; ?></b>
+                                    <input type="hidden" name="id" value="<?= $id; ?>">
                                   </div>
                                   <!-- Modal Footer -->
                                   <div class="modal-footer">
                                     <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Cancel</button>
-                                    <button type="submit" class="btn btn-danger" name="hapusGallery">Delete</button>
+                                    <button type="submit" class="btn btn-danger" name="deleteGallery">Delete</button>
                                   </div>
                                 </form>
                               </div>
